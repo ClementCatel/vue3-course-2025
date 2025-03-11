04b68b9e3590c884a702c25c0939a46737b7b61f
Clement Catel <clement.catel4@gmail.com>
2025-03-11 22:49:38 +0100

    add post actions

--- src/components/AppPost.vue
+++ src/components/AppPost.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { TrashIcon } from "@heroicons/vue/16/solid";
+import { TrashIcon, PencilSquareIcon, HeartIcon } from "@heroicons/vue/24/outline";
+import { HeartIcon as SolidHeartIcon } from "@heroicons/vue/24/solid";
 import { useRouter } from "vue-router"
 
 const router = useRouter();
 
 defineProps(["post"]);
-const emit = defineEmits(["delete"]);
+const emit = defineEmits(["delete", "like"]);
 
 const goToUserProfile = () => {
   router.push({ name: "user-profile"});
@@ -14,70 +15,33 @@ const goToUserProfile = () => {
 
 <template>
   <article class="card">
-    <img
-      :src="post.user.avatar"
-      :alt="`${post.user.username} avatar picture`"
-      width="36"
-      height="36"
-      class="user-avatar"
-    >
-    <div class="post-content">
-      <header class="post-header">
-        <a @click="goToUserProfile" class="user-link">{{ post.user.username }}</a>
-        <button class="delete-post" @click="emit('delete', post.id)">
-          <TrashIcon />
-        </button>
-      </header>
-      <p>{{ post.text }}</p>
-    </div>
+    <header>
+      <img
+        :src="post.user.avatar"
+        :alt="`${post.user.username} avatar picture`"
+        width="36"
+        height="36"
+        class="avatar"
+      >
+      <a @click="goToUserProfile">{{ post.user.username }}</a>
+      <small>{{ new Date(post.createdAt).toLocaleDateString() }}</small>
+    </header>
+
+    <p>{{ post.text }}</p>
+
+    <footer>
+      <button class="btn-icon" :class="{'active': post.liked}" @click="() => emit('like', post.id)">
+        <component :is="post.liked ? SolidHeartIcon : HeartIcon" />
+        <!-- <SolidHeartIcon v-if="post.liked" />
+        <HeartIcon v-else /> -->
+      </button>
+      <div class="spacer"></div>
+      <button class="btn-icon">
+        <PencilSquareIcon />
+      </button>
+      <button @click="() => emit('delete', post.id)" class="btn-icon">
+        <TrashIcon />
+      </button>
+    </footer>
   </article>
 </template>
-
-<style scoped>
-article {
-  display: flex;
-  gap: 0.75rem;
-  padding: 0.75rem 1.5rem;
-  overflow: hidden;
-}
-article p {
-  white-space: pre-wrap;
-}
-article:hover .delete-post {
-  display: block;
-}
-
-.post-content {
-  overflow: hidden;
-  width: 100%;
-}
-.post-header {
-  display: flex;
-  justify-content: space-between;
-}
-
-.user-avatar {
-  border-radius: 50%;
-  margin-top: 0.25rem;
-  object-fit: cover;
-}
-.user-link {
-  cursor: pointer;
-  font-weight: bold;
-}
-.user-link:hover {
-  text-decoration: underline;
-}
-
-.delete-post {
-  background: none;
-  border: none;
-  cursor: pointer;
-  display: none;
-  width: 24px;
-  color: var(--color-text-primary);
-}
-.delete-post:hover {
-  color: var(--color-accent);
-}
-</style>
